4bcef2e88b30debd3e277e23f338f4705140a4fb
Nahid2194 <nahidjc80@gmail.com>
2020-11-13 10:09:11 +0600

    sign out

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../App';
 import logo from '../../images/logo.png'
 import './Header.css';
 const Header = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
     return (
 
         <div className="header">
@@ -11,8 +14,9 @@ const Header = () => {
                 <Link to="/shop">Shop</Link>
                 <Link to="/review">Order Review</Link>
                 <Link to="/manage">Manage Inventory</Link>
+                <button onClick={() => setLoggedInUser({})}>Sign Out</button>
             </nav>
-        </div>
+        </div >
     );
 };
 
